92b51b004455e81d2182780666b7db3a7df61387
Flaviobrdf <flaviobrdf@hotmail.com>
2023-04-18 00:45:23 -0300

    add aula 02

---
+++ Java-T3/Aula-02/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Word");
+    }
+}
+
+class Cachorro {
+    String race;
+    Float size;
+    String color;
+    int age;
+    String name;
+}
